8ebc2397f2193983361b609240b5b596a7d835bd
dan-dan97 <danielv441@gmail.com>
2018-03-13 16:12:14 +0300

    Excluded device path constructor

--- examples/keyboard_lib_test.cpp
+++ examples/keyboard_lib_test.cpp
@@ -9,7 +9,6 @@ int main (int argc, char *argv[])
 {
     //Keyboard keyboard(0); //Number of keyboard beginning from zero. Performs searching devices with "keyboard" in their discription with the command "lsinput" (needs sudo apt-get install input-utils)
     Keyboard keyboard; //First found keyboard
-    //Keyboard keyboard("/dev/input/event14"); //Opening manually the keyboard file, so we don`t need to perform the search (it takes for about 1 second)
 
     //Use key codes from file "linux/input.h", they are different from ASCII codes
 
